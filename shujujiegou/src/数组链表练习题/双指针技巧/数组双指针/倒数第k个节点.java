22e2aee76505bca92a125c890455722ae9478385
Hejiusan <hekang@qq.com>
2024-04-08 23:59:49 +0800

    双指针

---
+++ shujujiegou/src/数组链表练习题/双指针技巧/数组双指针/倒数第k个节点.java
@@ -0,0 +1,16 @@
+package 数组链表练习题.双指针技巧.数组双指针;
+
+public class 倒数第k个节点 {
+
+    public int kthToLast(ListNode head, int k) {
+
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+}
+
