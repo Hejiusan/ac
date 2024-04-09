a638e5835200ce50d99217abfe1ed8ae06137cf8
Hejiusan <hekang@qq.com>
2024-04-10 00:08:38 +0800

    双指针

--- shujujiegou/src/数组链表练习题/双指针技巧/数组双指针/倒数第k个节点.java
+++ shujujiegou/src/数组链表练习题/双指针技巧/数组双指针/倒数第k个节点.java
@@ -1,9 +1,24 @@
 package 数组链表练习题.双指针技巧.数组双指针;
 
+/**
+ * https://leetcode.cn/problems/kth-node-from-end-of-list-lcci/submissions/521923401/
+ */
 public class 倒数第k个节点 {
 
     public int kthToLast(ListNode head, int k) {
+        ListNode p1 = head; // p1指向头结点
+        // P1先走k步
+        for (int i = 0; i < k; i++) {
+            p1 = p1.next;
+        }
 
+        ListNode p2 = head;
+        // p1 p2一起走 当p1指向null  p2恰好停在n-k+1的位置
+        while (p1!=null){
+            p2=p2.next;
+            p1=p1.next;
+        }
+        return p2.val;
     }
 
     public class ListNode {
--- shujujiegou/src/数组链表练习题/双指针技巧/链表双指针/_876_链表的中间结点.java
+++ shujujiegou/src/数组链表练习题/双指针技巧/链表双指针/_876_链表的中间结点.java
@@ -2,6 +2,8 @@ package 数组链表练习题.双指针技巧.链表双指针;
 
 /**
  *  https://leetcode.cn/problems/middle-of-the-linked-list/
+ *  给你单链表的头结点 head ，请你找出并返回链表的中间结点。
+ * 如果有两个中间结点，则返回第二个中间结点。
  */
 public class _876_链表的中间结点 {
     class Solution {
