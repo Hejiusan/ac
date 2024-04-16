396ec3f8cfb284f6eabf58fa50783fb0805d7f4b
何康 <hekang@qq.com>
2024-04-16 17:59:18 +0800

    二叉树遍历

--- shujujiegou/src/二叉树/遍历/_104_二叉树的最大深度.java
+++ shujujiegou/src/二叉树/遍历/_104_二叉树的最大深度.java
@@ -1,2 +1,73 @@
-package 二叉树.遍历;public class _104_二叉树的最大深度 {
+package 二叉树.遍历;
+
+/**
+ * https://leetcode.cn/problems/maximum-depth-of-binary-tree/description/
+ * 给定一个二叉树 root ，返回其最大深度。
+ * 二叉树的 最大深度 是指从根节点到最远叶子节点的最长路径上的节点数。
+ */
+public class _104_二叉树的最大深度 {
+
+    // 记录最大深度
+    int res = 0;
+    // 记录遍历到的节点的深度
+    int depth = 0;
+
+    // 主函数
+    public int maxDepth(TreeNode root) {
+        traverse(root);
+        return res;
+    }
+
+    // 二叉树遍历框架
+    void traverse(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        // 前序位置
+        depth++;
+        if (root.left == null && root.right == null) {
+            // 到达叶子节点，更新最大深度
+            res = Math.max(res, depth);
+        }
+        traverse(root.left);
+        traverse(root.right);
+        // 后序位置
+        depth--;    // 这里-- 是因为你遍历完左子树，回到右子树的根节点，深度-1
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+
+    // 法二  直接通过子树的最大深度来推出树的最大深度
+    // 定义：输入根节点，返回这棵二叉树的最大深度
+    int maxDepth2(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        // 利用定义，计算左右子树的最大深度
+        int leftMax = maxDepth2(root.left);
+        int rightMax = maxDepth2(root.right);
+        // 整棵树的最大深度等于左右子树的最大深度取最大值，
+        // 然后再加上根节点自己
+        int res = Math.max(leftMax, rightMax) + 1;
+
+        return res;
+    }
 }
--- shujujiegou/src/二叉树/遍历/_144_二叉树的前序遍历.java
+++ shujujiegou/src/二叉树/遍历/_144_二叉树的前序遍历.java
@@ -1,2 +1,45 @@
-package 二叉树.遍历;public class _144_二叉树的前序遍历 {
+package 二叉树.遍历;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * https://leetcode.cn/problems/binary-tree-preorder-traversal/description/
+ */
+public class _144_二叉树的前序遍历 {
+    List<Integer> res = new LinkedList<>();
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        traverse(root);
+        return res;
+    }
+    // 二叉树遍历函数
+    void traverse(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        // 前序位置
+        res.add(root.val);
+        traverse(root.left);
+        traverse(root.right);
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 }
