6b0944b67c02ca657fa906d25792bfeabf928cf7
hekang <hekang@qq.com>
2024-05-30 17:58:54 +0800

    完全背包

---
+++ shujujiegou/src/动态规划/背包问题/_518_零钱兑换II.java
@@ -0,0 +1,27 @@
+package 动态规划.背包问题;
+
+/**
+ * https://leetcode.cn/problems/coin-change-ii/description/
+ *
+ * 给你一个整数数组 coins 表示不同面额的硬币，另给一个整数 amount 表示总金额。
+ * 请你计算并返回可以凑成总金额的硬币组合数。如果任何硬币组合都无法凑出总金额，返回 0 。
+ * 假设每一种面额的硬币有无限个。
+ * 题目数据保证结果符合 32 位带符号整数。
+ */
+public class _518_零钱兑换II {
+    public int change(int amount, int[] coins) {
+        // dp[j]: 凑成总金额j的货币组合数为dp[j]
+        // dp[j] 就是所有的dp[j - coins[i]]（考虑coins[i]的情况）相加
+        // 如果我们要凑成金额 j，我们可以将当前硬币 coin 加入到凑成金额 j - coin 的组合中。
+        // 转移方程：dp[j] += dp[j - coins[i]];
+        int[] dp = new int[amount+1];
+        dp[0] = 1;
+        for (int i = 0; i < coins.length; i++) { // 遍历物品
+            for (int j = coins[i]; j <= amount; j++) { // 遍历背包
+                dp[j] += dp[j - coins[i]];
+            }
+        }
+
+        return dp[amount]; // 返回凑成金额 amount 的组合数
+    }
+}
---
+++ shujujiegou/src/动态规划/背包问题/完全背包.java
@@ -0,0 +1,29 @@
+package 动态规划.背包问题;
+
+/**
+ * 有N件物品和一个最多能背重量为W的背包。第i件物品的重量是weight[i]，得到的价值是value[i] 。
+ * 每件物品都有无限个（也就是可以放入背包多次），求解将哪些物品装入背包里物品价值总和最大。
+ * 完全背包和01背包问题唯一不同的地方就是，每种物品有无限件。
+ */
+public class 完全背包 {
+
+
+    //先遍历背包，再遍历物品
+    private static void testCompletePackAnotherWay(){
+        int[] weight = {1, 3, 4};
+        int[] value = {15, 20, 30};
+        int bagWeight = 4;
+        int[] dp = new int[bagWeight + 1];  //dp[j] 表示背包容量为j所能装的最大价值
+        for (int i = 1; i <= bagWeight; i++){ // 遍历背包容量
+            for (int j = 0; j < weight.length; j++){ // 遍历物品
+                if (i - weight[j] >= 0){
+                    dp[i] = Math.max(dp[i], dp[i - weight[j]] + value[j]);
+                }
+            }
+        }
+        for (int maxValue : dp){
+            System.out.println(maxValue + "   ");
+        }
+    }
+
+}
